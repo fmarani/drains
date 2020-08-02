f81bcccd2b0ad93409a81897bb80c32e344b1e27
Federico Marani <flagzeta@gmail.com>
2020-08-02 18:17:15 +0200

    Added limit parameter, fixed tests

--- drains/middleware.py
+++ drains/middleware.py
@@ -1,5 +1,10 @@
 import aioredis
 import asyncio
+from urllib.parse import parse_qs
+
+
+class LimitReached(Exception):
+    pass
 
 
 class SSEEndpoint:
@@ -10,7 +15,7 @@ class SSEEndpoint:
     async def create_redis_conn(self):
         return await aioredis.create_redis('redis://localhost/0')
 
-    async def redis_loop(self, channel_name, stream_from, send):
+    async def redis_loop(self, channel_name, stream_from, limit_events, send):
         print("creating redis listener")
         redis = await self.create_redis_conn()
 
@@ -28,9 +33,11 @@ class SSEEndpoint:
                         'more_body': True
                     })
             print(f"awaiting items from {channel_name}")
+            c = 0
             while results := await redis.xread([channel_name]):
                 print(f"got results from {channel_name}")
                 for result in results:
+                    c += 1
                     stream_name, stream_id, msg = result
                     msg = dict(msg)
                     print(stream_id)
@@ -40,12 +47,14 @@ class SSEEndpoint:
                         'body': b"event: " + stream_id + b"|" + msg[b'message'] + b"\r\n",
                         'more_body': True
                     })
-        except asyncio.CancelledError:
+                    if limit_events and c >= limit_events:
+                        print("hitting limit")
+                        raise LimitReached("returned enough events")
+        except (asyncio.CancelledError, LimitReached):
             redis.close()
-            await redis.wait_closed()
             await send({
                 'type': 'http.response.body',
-                'body': '',
+                'body': b'',
             })
 
     async def __call__(self, scope, receive, send):
@@ -57,7 +66,15 @@ class SSEEndpoint:
             channel_name = scope['path'][len(self.url_prefix):].strip("/")
             if channel_name == "":
                 raise Exception("channel name must be not empty")
-            stream_from = scope['query_string']
+
+            query_string = parse_qs(scope['query_string'])
+            stream_from = limit_events = None
+
+            if query_string.get(b'from'):
+                stream_from = query_string.get(b'from')[0]
+            if query_string.get(b'limit'):
+                limit_events = int(query_string.get(b'limit')[0])
+
             await send({
                 'type': 'http.response.start',
                 'status': 200,
@@ -69,17 +86,23 @@ class SSEEndpoint:
 
             print("creating sender")
 
-            receiver = receive()
-            looper = self.redis_loop(channel_name, stream_from, send)
+            looper = self.redis_loop(channel_name, stream_from, limit_events, send)
             looper_task = asyncio.create_task(looper)
 
-            while True:
-                done, _ = await asyncio.wait([receiver])
-                if receiver in done:
-                    event = await receiver
-                    if event['type'] == "http.disconnect":
-                        break
-                await asyncio.sleep(0.5)
+            try:
+                while True:
+                    done, still = await asyncio.wait([receive()])
+                    for d in done:
+                        event = await d
+                        print("event", event)
+                        if event['type'] == "http.disconnect":
+                            print("client disconnecting")
+                            raise IOError
+                    await asyncio.sleep(0.5)
+            except IOError:
+                pass
+            except asyncio.CancelledError:
+                print("wait cancelled")
 
             print("cancelling sender")
             looper_task.cancel()
--- tests/test_request.py
+++ tests/test_request.py
@@ -6,17 +6,41 @@ import asyncio
 from .asgi_app import app
 
 @pytest.mark.asyncio
-async def ztest_server_is_alive(xserver):
-    with xserver() as xserver:
-        client = httpx.AsyncClient()
-        resp = await client.get(xserver.http_base_url + "/")
+async def test_server_is_alive():
+    async with TestClient(app) as client:
+        resp = await client.get("/")
         assert resp.status_code == 200
 
 @pytest.mark.asyncio
-async def ztest_server_is_alive():
-    client = httpx.AsyncClient(app=app, base_url="http://testserver")
-    resp = await client.get("/")
-    assert resp.status_code == 200
+async def test_sse_events_work_with_limit():
+    async def emitter():
+        print("emitting to example")
+        await send_event_async("example", "payload1")
+        print("emitting to example")
+        await send_event_async("example", "payload2")
+        print("emitting to example")
+        await send_event_async("example", "payload3")
+
+    async def receiver():
+        async with TestClient(app) as client:
+            resp = await client.get("/sse/example/?limit=3", stream=True)
+            print("receiving")
+            assert resp.status_code == 200
+
+            line = 0
+            async for i in resp:
+                line += 1
+                print("received", line)
+                if i.startswith(b"event:"):
+                    assert f"payload{line}" in i.decode("utf8")
+
+        print("teard down client")
+
+
+    receiver_task = asyncio.create_task(receiver())
+    await asyncio.sleep(0.3)
+    await emitter()
+    await receiver_task
 
 @pytest.mark.asyncio
 async def test_sse_events_work():
@@ -38,14 +62,15 @@ async def test_sse_events_work():
             async for i in resp:
                 line += 1
                 print("received", line)
-                assert f"payload{line}" in i.decode("utf8")
+                if i.startswith(b"event:"):
+                    assert f"payload{line}" in i.decode("utf8")
                 if line == 3:
                     break
+
+        print("teard down client")
 
 
     receiver_task = asyncio.create_task(receiver())
     await asyncio.sleep(0.3)
     await emitter()
     await receiver_task
-
-    #await asyncio.gather(receiver_task, emitter_task)
